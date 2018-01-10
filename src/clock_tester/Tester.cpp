fb9ab2aad5eb63acb69acf3375bc24363ca1c276
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2018-01-10 19:07:06 +0100

    Add clock_tester example

---
+++ src/clock_tester/Tester.cpp
@@ -0,0 +1,41 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "Tester.h"
+
+namespace smile_examples {
+namespace clock_tester {
+
+Define_Module(Tester);
+
+void Tester::initialize(int stage)
+{
+  ClockDecorator<omnetpp::cSimpleModule>::initialize(stage);
+  if (stage == inet::INITSTAGE_LOCAL) {
+    auto message = std::make_unique<cMessage>("timer");
+    const auto timestamp = clockTime() + SimTime(1, omnetpp::SIMTIME_NS);
+    scheduleAt(timestamp, message.release());
+  }
+}
+
+void Tester::handleSelfMessage(cMessage* newMessage)
+{
+  std::unique_ptr<cMessage> message{newMessage};
+  const auto timestamp = clockTime() + SimTime(1, omnetpp::SIMTIME_NS);
+  scheduleAt(timestamp, message.release());
+}
+
+}  // namespace clock_tester
+}  // namespace smile_examples
---
+++ src/clock_tester/Tester.h
@@ -0,0 +1,41 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#pragma once
+
+#include <ClockDecorator.h>
+
+namespace smile_examples {
+namespace clock_tester {
+
+class Tester : public smile::ClockDecorator<omnetpp::cSimpleModule>
+{
+ public:
+  Tester() = default;
+  Tester(const Tester& source) = delete;
+  Tester(Tester&& source) = delete;
+  ~Tester() = default;
+
+  Tester& operator=(const Tester& source) = delete;
+  Tester& operator=(Tester&& source) = delete;
+
+ private:
+  void initialize(int stage) override;
+
+  void handleSelfMessage(cMessage* newMessage) override;
+};
+
+}  // namespace clock_tester
+}  // namespace smile_examples
